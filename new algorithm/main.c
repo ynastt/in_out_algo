4c2364d20d93062a3d794e6dc9d255bfa07afe41
ynastt <yarovikovaanastasia1@gmail.com>
2023-07-20 20:15:13 +0300

    refal structures

--- new algorithm/main.c
+++ new algorithm/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+/*** Refal structures. ***/
+typedef struct link {
+    char ptype; /* type of the link */  
+    union {
+        struct link *b; /* bracket: ptr to the pair */
+    char *f; /* function or compound symbol: ptr to label. */
+    char c; /* symbol: actual value. */
+    /* unsigned long u; /+* unicode symbol */
+    unsigned int u;  /* unicode symbol */
+    unsigned long n; /*  macro-digit   */
+    unsigned short us_1, us_2;
+    } pair;
+    struct link *prec; /* ptr to preceding link */
+    struct link *foll; /* ptr to following link */
+} LINK;
+
+#define NEXT(q)    ((q)->foll)
+#define PREV(q)    ((q)->prec)
+#define TYPE(q)    ((q)->ptype)
+
 int main()
 {
     long temp = 0;
