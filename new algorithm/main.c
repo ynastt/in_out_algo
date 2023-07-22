e89b6736f674fc5aaf4d2042d34a1c9b101b1992
ynastt <yarovikovaanastasia1@gmail.com>
2023-07-23 00:38:50 +0300

    octal

--- new algorithm/main.c
+++ new algorithm/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
 
 /*** Refal structures. ***/
 typedef struct link {
@@ -22,23 +25,83 @@ typedef struct link {
 #define PREV(q)    ((q)->prec)
 #define TYPE(q)    ((q)->ptype)
 
+
+
+void slice(const char* str, char* result, size_t start, size_t end)
+{
+    strncpy(result, str + start, end - start);
+}
+
+char * my_octal_add(code1, digits) {
+    char *code;
+
+    return code;
+}
+
 int main()
 {
-    long temp = 0;
-    int sum = 0;
+    unsigned long temp = 0;
+    clock_t t;
+    
     FILE * f = fopen("../tests/test1.txt", "rt");
+    char *code;
+    char *code1;
+    size_t n = 0, m = 0;
+    int c;
+
     if( f == NULL ) {
         perror("Error: ");
         return(-1);
     }
-    while ( fscanf(f, "%ld", &temp) == 1 ) {
-        sum += temp;
+
+    fseek(f, 0, SEEK_END);
+    long f_size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    printf(" size of file: %ld\n", f_size);
+
+    code = malloc(f_size);
+    code1 = malloc(f_size);
+
+    while ((c = fgetc(f)) != EOF) {
+        code[n++] = (char)c;
+        code1[m++] = (char)c;
+        // printf("char: %c\n", (char)c);
     }
-        
+
+    code[n] = '\0'; 
+    code1[m] = '\0'; 
+    int cursor = 0;
+    for (int i = 0; i < f_size; i++) {
+        cursor = i + 1;
+        printf("\ndigit char: %c\n", code1[i]);
+        char* todigits;
+        todigits = malloc(f_size);
+        slice(code1, todigits, 0, cursor);
+        printf("%s\n", code1);
+        printf("%s\n", todigits);
+        unsigned long long digits = (unsigned long long)atoi(todigits); 
+        printf("digit long: %lld\n", digits);
+        if (cursor == f_size) {
+            break;
+        }
+        digits = digits * 2 * powl(10, (f_size - 1 - cursor));
+        printf("digit * 2 step: %lld\n", digits);
+        code1 = my_octal_add(code1, digits); //todo
+
+    }
+     
     fclose(f);
+    printf("whole number: %s\n", code);
     
     FILE * o = fopen("output.txt", "w");
-    fprintf(o, "%d", sum);
-    fclose(o);
+    fputs(code, o);
+    fclose(o);    
+
+    t = clock();
+    
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    printf("algorithm took %f seconds to execute \n", time_taken);
+    free(code);
     return 0;
 }
